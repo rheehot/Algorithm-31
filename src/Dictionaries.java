cfa6e40555b4e873962a4b2b1e80b3170944b47c
wonhee <wonhee010@gmail.com>
2019-09-16 22:14:19 +0900

    dictionaries

--- src/Dictionaries.java
+++ src/Dictionaries.java
@@ -1,8 +1,38 @@
 
+/*
+ * https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem?h_r=next-challenge&h_v=zen
+ */
+
+import java.util.*;
 public class Dictionaries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		scan.nextLine();
+		Map<String, String> phoneBook = new HashMap<String, String>(); 
+		
+		String test = "";
+		
+		for(int i = 0; i < n; i++) {
+			test = scan.nextLine();
+			String[] words = test.split(" ");
+			
+			phoneBook.put(words[0], words[1]);
+		}
+		
+		String name = "";
+		
+		while(scan.hasNext()) {
+			name = scan.nextLine();
+			if (phoneBook.get(name) == null) {
+				System.out.println("Not found");
+			}
+			else {
+				System.out.println(name + "=" + phoneBook.get(name));
+			}
+		}
 
 	}
 
