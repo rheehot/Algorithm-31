77c227bf1cc18da5b170ed3a27492948f5c6199b
wonhee009 <wonhee010@gmaill.com>
2019-09-21 15:30:07 +0900

    edit main

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,6 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
 
-		int[][] array = new int[num][num];
-		for(int i = 0; i < num; i++) {
-			for(int j = 0; j < num; j++) {
-				array[i][j] = scan.nextInt();
-			}
-		}
-
-		int right = 0;
-		int left = 0;
-		for(int i = 0; i < num; i++) {
-			right += array[i][i];
-			left += array[i][num - i - 1];
-		}
-
-		System.out.println(Math.abs(right - left));
 	}
 }
