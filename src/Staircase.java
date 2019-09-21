5760758fee1093a2b5db9e74c5656ca67bd341b7
wonhee009 <wonhee010@gmaill.com>
2019-09-21 15:43:29 +0900

    add Staircase

---
+++ src/Staircase.java
@@ -0,0 +1,22 @@
+/*
+ * https://www.hackerrank.com/challenges/staircase/problem
+ */
+
+import java.util.*;
+public class Staircase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for(int j = i; j >0; j--) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
