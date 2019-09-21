5724cbe52e447770856703f049767f5b8ffe210f
wonhee <wonhee010@gmaill.com>
2019-09-21 14:57:08 +0900

    add plusminus

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-
 import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
-		int N = scan.nextInt();
-		int result = 1;
-		
-		for(int i = N; i > 0; i--) {
-			result *= i;
-		}
-		
-		System.out.println(result);
 	}
-
 }
---
+++ src/PlusMinus.java
@@ -0,0 +1,36 @@
+/*
+ * https://www.hackerrank.com/challenges/plus-minus/problem
+ */
+import java.util.*;
+public class PlusMinus {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        int[] array = new int[num];
+        double positive = 0;
+        double negative = 0;
+        double zero = 0;
+        for(int i = 0 ; i < num; i++) {
+            int temp = scan.nextInt();
+            if (temp == 0) {
+                zero++;
+            }
+            else if (temp < 0) {
+                negative++;
+            }
+            else {
+                positive++;
+            }
+        }
+
+        double l = Double.valueOf(num);
+        double p = positive / l;
+        System.out.println(String.format("%.6f", p));
+        double n = negative / l;
+        System.out.println(String.format("%.6f", n));
+        double z = zero / l;
+        System.out.println(String.format("%.6f", z));
+    }
+}
