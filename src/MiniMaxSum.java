96cd9f40ecdc0c34634d5fb720245c90c4f040b9
wonhee009 <wonhee010@gmaill.com>
2019-09-23 21:38:21 +0900

    edit Mini-Max Sum

--- src/MiniMaxSum.java
+++ src/MiniMaxSum.java
@@ -1,18 +1,27 @@
+/*
+ * https://www.hackerrank.com/challenges/mini-max-sum/problem
+ */
+
 import java.util.*;
 public class MiniMaxSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] array = new int[5];
+        long sum = 0;
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
         for(int i = 0; i < 5; i++) {
-            array[i] = scan.nextInt();
+            long num = scan.nextLong();
+            sum += num;
+
+            if (min > num) {
+                min = num;
+            }
+            if (max < num) {
+                max = num;
+            }
         }
 
-        Arrays.sort(array);
-
-        int min = array[0] + array[1] + array[2] + array[3];
-        int max = array[1] + array[2] + array[3] + array[4];
-
-        System.out.println(min + " " + max);
+        System.out.println((sum - max) + " " + (sum - min));
     }
 }
