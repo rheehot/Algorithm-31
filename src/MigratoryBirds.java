18ea54dfa682f870915bed14aa7642a93ee3664e
wonhee <wonhee010@gmail.com>
2019-10-05 14:30:11 +0900

    add migratory birds

---
+++ src/MigratoryBirds.java
@@ -0,0 +1,37 @@
+/*
+ * https://www.hackerrank.com/challenges/migratory-birds/problem
+ */
+
+import java.util.*;
+public class MigratoryBirds {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] birds = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            birds[i] = scan.nextInt();
+        }
+
+        System.out.print(migratoryBirds(birds));
+    }
+
+    static int migratoryBirds(int[] birds) {
+        int[] birdsType = new int[6];
+        for(int i = 0; i < birds.length; i++) {
+            birdsType[birds[i]]++;
+        }
+
+        int max = 0;
+        int maxIndex = -1;
+        for(int i = 5; i > 0; i--) {
+            if (birdsType[i] >= max) {
+                max = birdsType[i];
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+}
