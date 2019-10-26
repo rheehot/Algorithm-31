79e5123b45f1a2e67a0b7f76db50a4f8b4e8a21c
wonhee <wonhee010@gmail.com>
2019-10-26 13:47:43 +0900

    add Network

--- src/Programmers/Network.java
+++ src/Programmers/Network.java
@@ -6,31 +6,27 @@ package src.Programmers;
 
 import java.util.*;
 public class Network {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int n = scan.nextInt();
-        int computers[][] = new int[n][n];
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                computers[i][j] = scan.nextInt();
-            }
-        }
-
-        System.out.print(solution(n, computers));
-    }
-
+    static boolean visited[];
     public static int solution(int n, int[][] computers) {
         int answer = 0;
+
+        visited = new boolean[n];
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if (computers[i][j] != 0 && computers[j][i] != 0) {
-                    answer++;
-                }
+            if (!visited[i]) {
+                dfs(computers, i, n);
+                answer++;
             }
         }
 
         return answer;
+    }
+
+    public static void dfs(int[][] computers, int index, int n) {
+        visited[index] = true;
+        for(int j = 0; j < n; j++) {
+            if(!visited[j] && computers[index][j] == 1) {
+                dfs(computers, j, n);
+            }
+        }
     }
 }
