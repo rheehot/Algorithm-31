9a0b7d880da4a1aed3eb3312e719eb9c8bd396b4
wonhee009 <wonhee010@gmail.com>
2020-02-13 23:15:40 +0900

    edit BuyAndSell

--- src/LeetCode/BuyAndSell.java
+++ src/LeetCode/BuyAndSell.java
@@ -1,4 +1,11 @@
-class Solution {
+package src.LeetCode;
+
+/**
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+ */
+
+import java.util.*;
+class BuyAndSell {
     public int maxProfit(int[] prices) {
         if (prices.length < 2) {
             return 0;
