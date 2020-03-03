8ee5a7f4339f822555bddf4715486913811e7680
wonhee <wonhee010@gmail.com>
2020-03-03 21:25:37 +0900

    add numbersum

--- src/BACKJOON/NumberSum.java
+++ src/BACKJOON/NumberSum.java
@@ -1,4 +1,19 @@
 package BACKJOON;
 
+import java.util.*;
+
 public class NumberSum {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String num = scan.next();
+
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += num.charAt(i) -'0';
+        }
+
+        System.out.println(result);
+    }
 }
