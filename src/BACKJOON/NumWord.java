b0860781d9478e499fa8d13aaccc616b152d8145
wonhee <wonhee010@gmail.com>
2020-03-07 17:29:41 +0900

    edit numword

--- src/BACKJOON/NumWord.java
+++ src/BACKJOON/NumWord.java
@@ -1,4 +1,13 @@
 package BACKJOON;
 
+import java.util.Scanner;
+
 public class NumWord {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        String[] result = str.split(" ");
+        System.out.println(result.length);
+    }
 }
