c4c57fea3f71e81b936d1689a3578093277fd72e
wonhee <wonhee010@gmail.com>
2020-03-10 21:01:09 +0900

    add dial

--- src/BACKJOON/Dial.java
+++ src/BACKJOON/Dial.java
@@ -1,7 +1,22 @@
 package BACKJOON;
 
+import java.util.Scanner;
+
 public class Dial {
+    static int[] sec =  {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+        int result = 0;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'A';
+
+            result += sec[index];
+        }
+
+        System.out.println(result);
     }
 }
