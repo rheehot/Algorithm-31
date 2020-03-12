ef2234ba22974db5d73db1e6fbfe4977e0b4abb4
wonhee <wonhee010@gmail.com>
2020-03-12 21:14:34 +0900

    add Snail

---
+++ src/BACKJOON/Snail.java
@@ -0,0 +1,29 @@
+package BACKJOON;
+
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int V = scan.nextInt();
+        V -= A;
+
+        if (V <= 0) {
+            System.out.println(1);
+        }
+        else {
+
+            int day = (V / (A - B));
+
+            if ((V % (A - B)) == 0) {
+                System.out.println(day + 1);
+            }
+            else {
+                System.out.println(day + 2);
+            }
+        }
+    }
+}
