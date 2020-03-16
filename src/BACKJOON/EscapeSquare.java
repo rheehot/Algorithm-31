f328977245d5856ace522d33afd2e155c07b8457
wonhee <wonhee010@gmail.com>
2020-03-16 21:16:32 +0900

    add EscapeSquare

---
+++ src/BACKJOON/EscapeSquare.java
@@ -0,0 +1,16 @@
+package BACKJOON;
+
+import java.util.Scanner;
+
+public class EscapeSquare {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int w = scan.nextInt();
+        int h = scan.nextInt();
+
+        System.out.println(Math.min(Math.min(w - x, x), Math.min(h - y, y)));
+    }
+}
