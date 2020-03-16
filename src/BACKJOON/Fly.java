5027bc1fd712f8cb660c2d6c5b6c7e126fa9416a
wonhee <wonhee010@gmail.com>
2020-03-16 20:28:31 +0900

    edit fly

--- src/BACKJOON/Fly.java
+++ src/BACKJOON/Fly.java
@@ -10,15 +10,21 @@ public class Fly {
         for(int i = 0; i < T; i++) {
             int x = scan.nextInt();
             int y = scan.nextInt();
-            int distance = y - x;
-            int result = 1;
-            int position = 0;
 
-            while(distance > position) {
-                result++;
-                position += distance / 2;
-            }
-            System.out.println(result - 1);
+            System.out.println(getMinMove(y - x));
+        }
+    }
+
+    public static int getMinMove(int n) {
+        // i: n 제곱근의 올림값
+        // i * i - i 보다 클 경우 2 * i - 1
+        // i * i - i 보다 작을 경우 2 * i - 2
+
+        int i = (int) Math.ceil(Math.sqrt(n));
+        if(n > i * i - i) {
+            return 2 * i - 1;
+        } else {
+            return 2 * i - 2;
         }
     }
 }
