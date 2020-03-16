4b37693d987074cfd0f0ad0f793adb1915fe82f2
wonhee <wonhee010@gmail.com>
2020-03-16 21:38:04 +0900

    add PYNumber

---
+++ src/Programmers/PYNumber.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+public class PYNumber {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+
+        int pNum = 0;
+        int yNum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'p') {
+                pNum++;
+            }
+            if (c == 'y') {
+                yNum++;
+            }
+        }
+        if (pNum == yNum) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
