774736b5b352310af2a25bb5c52cef3687fa53d2
wonhee <wonhee010@gmail.com>
2020-03-25 22:15:04 +0900

    add zero

--- src/BACKJOON/Zero.java
+++ src/BACKJOON/Zero.java
@@ -1,9 +1,29 @@
 package BACKJOON;
 
-import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Zero {
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args)  {
+        Scanner scan = new Scanner(System.in);
+        int K = scan.nextInt();
+        Stack<Integer> s = new Stack<Integer>();
+        for(int i = 0; i < K; i++) {
+            int m = scan.nextInt();
+
+            if (m == 0) {
+                s.pop();
+            }
+            else {
+                s.push(m);
+            }
+        }
+
+        int result = 0;
+        while(!s.isEmpty()) {
+            result += s.pop();
+        }
+
+        System.out.println(result);
     }
 }
