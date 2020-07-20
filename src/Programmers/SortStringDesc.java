bac918d6909d278f6207051026f7efb644530921
wonhee <wonhee010@gmail.com>
2020-07-20 14:34:30 +0900

    Create SortStringDesc.java

---
+++ src/Programmers/SortStringDesc.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        
+        char[] temp = new char[s.length()];
+        for(int index = 0; index < s.length(); index++) {
+            temp[index] = s.charAt(index);
+        }
+        Arrays.sort(temp);
+        String answer = "";
+        for(int index = temp.length - 1; index >= 0; index--) {
+            answer += temp[index];
+        }
+        return answer;
+    }
+}
