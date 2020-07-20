d3b2828781e3eee451845308dfc6b3b30b257e7f
wonhee <wonhee010@gmail.com>
2020-07-20 14:16:51 +0900

    Create CountPY.java

---
+++ src/Programmers/CountPY.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+         s = s.toLowerCase();
+
+        int pNum = 0;
+        int yNum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'p') {
+                pNum++;
+            }
+            if (c == 'y') {
+                yNum++;
+            }
+        }
+        if (pNum == yNum) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
