291424844296d7a30af40d680cb4b4390cfe037d
wonhee <wonhee010@gmail.com>
2020-07-20 13:36:32 +0900

    Create TwoIntSum.java

---
+++ src/Programmers/TwoIntSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(int a, int b) {
+        
+        if(a == b) {
+            return a;
+        }
+        int start = 0;
+        int end = 0;
+        if (a < b) {
+            start = a;
+            end = b;
+        }
+        else {
+            start = b;
+            end = a;
+        }
+        
+        long answer = 0;
+        for(int i = start; i <= end; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
