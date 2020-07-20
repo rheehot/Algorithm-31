83c438909cb21fc29ab36eead11769bc2e463446
wonhee <wonhee010@gmail.com>
2020-07-20 13:56:42 +0900

    Create CustomSort.java

---
+++ src/Programmers/CustomSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                char c1 = s1.charAt(n);
+                char c2 = s2.charAt(n);
+                
+                if(c1 == c2) {
+                    return s1.compareTo(s2);
+                }
+                
+                if(c1 < c2) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
+            }
+        });
+        return strings;
+    }
+}
