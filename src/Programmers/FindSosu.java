a39b36281ab463cdca3f79ad14bf0173c17a712c
wonhee <wonhee010@gmail.com>
2020-07-21 12:40:49 +0900

    Create FindSosu.java

---
+++ src/Programmers/FindSosu.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] sosu = new boolean[n + 1];
+        for(int i = 2; i < sosu.length; i++) {
+            if(!sosu[i]) {
+                answer++;
+                
+                int temp = 2;
+                while(true) {
+                    if (i * temp >= sosu.length) {
+                        break;
+                    }
+                    sosu[i * temp] = true;
+                    temp++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
