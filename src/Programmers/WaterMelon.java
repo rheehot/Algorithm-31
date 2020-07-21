f20c3cc2ec57c76d14b4da2317847bb6805d02d6
wonhee <wonhee010@gmail.com>
2020-07-21 12:51:25 +0900

    Create WaterMelon.java

---
+++ src/Programmers/WaterMelon.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String format = "수박";
+    
+        String answer = "";
+        for(int i = 0; i < n / 2; i++) {
+            answer += format;
+        }
+        
+        if(n % 2 != 0) {
+            answer += "수";
+        }
+        return answer;
+    }
+}
