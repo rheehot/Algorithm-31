5c72782c46661cc8f123e9f896d88f236a1169d2
wonhee <wonhee010@gmail.com>
2020-07-24 15:08:25 +0900

    Create ReverseArr.java

---
+++ src/Programmers/ReverseArr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(long n) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        
+        while(n > 0) {
+            int t = (int) (n % 10);
+            temp.add(t);
+            n /= 10;
+        }
+        
+        int[] answer = new int[temp.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = temp.get(i);
+        }
+        
+        return answer;
+    }
+}
