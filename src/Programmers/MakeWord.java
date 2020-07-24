cb5f79b647c1f6cb61a4a96327194d5c7658c3fb
wonhee <wonhee010@gmail.com>
2020-07-24 14:32:51 +0900

    Create MakeWord.java

---
+++ src/Programmers/MakeWord.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        s = s.replace(" ", "/");
+    
+        String answer = "";
+        int gap = 'a' - 'A';
+        int i = 0;
+        for(int index = 0; index < s.length(); index++) {
+            char temp = s.charAt(index);
+            if (temp == '/') {
+                answer += " ";
+                i = 0;
+                continue;
+            }
+            
+            // 짝수
+            if (i % 2 == 0) {
+                if (temp >= 'a' && temp <= 'z') {
+                    temp -= gap;
+                }
+            }
+            // 홀수
+            else {
+                if (temp >= 'A' && temp <= 'Z') {
+                    temp += 32;
+                }
+            }
+            answer += temp;
+            i++;
+        }
+        return answer;
+    }
+}
