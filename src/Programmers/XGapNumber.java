6586b89ac696a4b797553364dc064851cc0c3340
wonhee <wonhee010@gmail.com>
2020-07-27 14:14:03 +0900

    Create XGapNumber.java

---
+++ src/Programmers/XGapNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+        
+        for(int index = 1; index < n; index++) {
+            answer[index] = answer[index - 1] + x;
+        }
+        
+        return answer;
+    }
+}
