3ca6b2d02110795ed5dbdb97d3215b347736539c
wonhee <wonhee010@gmail.com>
2020-07-27 13:49:03 +0900

    Create HidePhoneNumber.java

---
+++ src/Programmers/HidePhoneNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int len = phone_number.length();
+        for(int index = 0; index < len; index++) {
+            if(index < len - 4) {
+                answer += "*";
+            }
+            else {
+                answer += phone_number.charAt(index);
+            }
+        }
+        
+        return answer;
+    }
+}
