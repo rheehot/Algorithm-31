98dfc348e4056e31c6a312feea3661fab20eec36
wonhee <wonhee010@gmail.com>
2020-08-24 22:14:01 +0900

    add Backjoon

---
+++ src/BACKJOON/SortNumberTree.java
@@ -0,0 +1,32 @@
+package BACKJOON;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class SortNumberTree {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > 0) {
+                for(int j = 0; j < arr[i]; j++) {
+                    bw.write(Integer.toString(i) + "\n");
+                }
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+}
--- src/BACKJOON/TreeTraversal.java
+++ src/BACKJOON/TreeTraversal.java
@@ -1,7 +1,78 @@
 package BACKJOON;
 
+import java.util.*;
+
+class Node {
+    char data;
+     Node left, right;
+
+     public Node(char data) {
+         this.data = data;
+     }
+}
+
+class TreeTraver {
+    Node root;
+
+    public void add(char data, char leftData, char rightData) {
+        if(root == null) {
+            if(data != '.') root = new Node(data);
+            if(leftData != '.') root.left = new Node(leftData);
+            if(rightData != '.') root.right = new Node(rightData);
+        }
+        else search(root, data, leftData, rightData);
+    }
+    public void search(Node root, char data, char leftData, char rightData) {
+         if(root == null) return;
+         else if(root.data == data) {
+             if(leftData != '.') root.left = new Node(leftData);
+             if(rightData != '.') root.right = new Node(rightData);
+         }
+         else {
+             search(root.left, data, leftData, rightData);
+             search(root.right, data, leftData, rightData);
+         }
+    }
+    public void preorder(Node root) {
+        System.out.print(root.data);
+        if(root.left != null) preorder(root.left);
+        if(root.right != null) preorder(root.right);
+    }
+    public void inorder(Node root) {
+        if(root.left != null) inorder(root.left);
+        System.out.print(root.data);
+        if(root.right != null) inorder(root.right);
+    }
+    public void postorder(Node root) {
+        if(root.left != null) postorder(root.left);
+        if(root.right != null) postorder(root.right);
+        System.out.print(root.data);
+    }
+}
+
 public class TreeTraversal {
+
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        scan.nextLine();
+
+        TreeTraver t = new TreeTraver();
+
+        for(int i = 1; i <= N; i++) {
+            String temp = scan.nextLine();
+
+            char node = temp.charAt(0);
+            char left = temp.charAt(2);
+            char right = temp.charAt(4);
+
+            t.add(node, left, right);
+        }
+
+        t.preorder(t.root);
+        System.out.println();
+        t.inorder(t.root);
+        System.out.println();
+        t.postorder(t.root);
     }
 }
