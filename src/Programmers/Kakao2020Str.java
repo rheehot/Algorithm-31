65388e8390701ae1cfd3f7322a342382eda0b12b
wonhee <wonhee010@gmail.com>
2020-09-11 13:17:12 +0900

    add solved

--- src/Programmers/Kakao2020Str.java
+++ src/Programmers/Kakao2020Str.java
@@ -5,12 +5,10 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Kakao2019Matching {
+public class Kakao2020Str {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
 
         bw.flush();
         bw.close();
