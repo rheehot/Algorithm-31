699c90d7df9c8d4b1af4e7ba149f27ad85e96639
wonhee <wonhee010@gmail.com>
2020-10-04 14:54:49 +0900

    add Solved

--- src/BACKJOON/BOJ2098.java
+++ src/BACKJOON/BOJ2098.java
@@ -1,4 +1,19 @@
 package BACKJOON;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class BOJ2098 {
+    static int n;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+
+        bw.flush();
+        bw.close();
+    }
 }
